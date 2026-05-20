4688cbda297ef6b9b01b8ad27a521d06331e95bc
artomily <vararakya@gmail.com>
2026-05-20 16:37:59 +0700

    Commit 23: Add landing route group with home page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,23 @@
+import { Navbar } from '@/app/components/landing/Navbar'
+import { HeroSection } from '@/app/components/landing/HeroSection'
+import { HowItWorks } from '@/app/components/landing/HowItWorks'
+import { FeaturesSection } from '@/app/components/landing/FeaturesSection'
+import { Testimonials } from '@/app/components/landing/Testimonials'
+import { EarningsTicker } from '@/app/components/landing/EarningsTicker'
+import { CTASection } from '@/app/components/landing/CTASection'
+import { Footer } from '@/app/components/landing/Footer'
+
+export default function LandingPage() {
+  return (
+    <div className="flex min-h-dvh flex-col bg-bg text-primary">
+      <Navbar />
+      <HeroSection />
+      <EarningsTicker />
+      <HowItWorks />
+      <FeaturesSection />
+      <Testimonials />
+      <CTASection />
+      <Footer />
+    </div>
+  )
+}
