89b951645cb5a54784b1407ccc42d8fdf69c5a3b
artomily <vararakya@gmail.com>
2026-05-20 13:36:12 +0700

    feat: update EarningsPage layout and remove ConnectGuard component

--- app/earnings/page.tsx
+++ app/earnings/page.tsx
@@ -1,19 +1,16 @@
-import { ConnectGuard } from '@/app/components/ConnectGuard'
 import { EarningsList } from '@/app/components/EarningsList'
 import { RewardPoolBalance } from '@/app/components/RewardPoolBalance'
 import { EarningsHeader } from '@/app/components/EarningsHeader'
 
 export default function EarningsPage() {
   return (
-    <ConnectGuard>
-      <div className="flex min-h-dvh flex-col bg-bg">
-        <EarningsHeader />
+    <div className="flex min-h-dvh flex-col bg-bg">
+      <EarningsHeader />
 
-        <div className="mx-auto w-full max-w-lg space-y-4 px-4 py-5 pb-24">
-          <RewardPoolBalance />
-          <EarningsList />
-        </div>
+      <div className="mx-auto w-full max-w-lg space-y-4 px-4 py-5 pb-24">
+        <RewardPoolBalance />
+        <EarningsList />
       </div>
-    </ConnectGuard>
+    </div>
   )
 }
