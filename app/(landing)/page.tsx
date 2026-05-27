e9cd475696d3eb83259a12796550cd937de3624a
artomily <vararakya@gmail.com>
2026-05-28 01:45:40 +0700

    Update landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,26 +1,28 @@
 import { Navbar } from '@/app/components/landing/Navbar'
 import { HeroSection } from '@/app/components/landing/HeroSection'
 import { HowItWorks } from '@/app/components/landing/HowItWorks'
+import { FeaturedVideo } from '@/app/components/landing/FeaturedVideo'
 import { FeaturesSection } from '@/app/components/landing/FeaturesSection'
 import { Testimonials } from '@/app/components/landing/Testimonials'
-import { EarningsTicker } from '@/app/components/landing/EarningsTicker'
+import { FAQ } from '@/app/components/landing/FAQ'
+import { SeeItInAction } from '@/app/components/landing/SeeItInAction'
 import { CTASection } from '@/app/components/landing/CTASection'
+import { Partners } from '@/app/components/landing/Partners'
 import { Footer } from '@/app/components/landing/Footer'
-import { FeaturedVideo } from '@/app/components/landing/FeaturedVideo'
-import { Tokenomics } from '@/app/components/landing/Tokenomics'
 
 export default function LandingPage() {
   return (
     <div className="flex min-h-dvh flex-col bg-bg text-primary">
       <Navbar />
       <HeroSection />
-      <FeaturedVideo />
-      <EarningsTicker />
       <HowItWorks />
+      <FeaturedVideo />
       <FeaturesSection />
-      <Tokenomics />
       <Testimonials />
+      <FAQ />
+      <SeeItInAction />
       <CTASection />
+      <Partners />
       <Footer />
     </div>
   )
